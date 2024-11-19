a66fc6f7c777d9c63fc1c20192f79293987aa038
hjlee-kr <sandjtech@naver.com>
2024-11-19 10:39:11 +0900

    db테스트 with mybatis

---
+++ src/main/java/com/jjangplay/mapper/TimeMapper.java
@@ -0,0 +1,13 @@
+package com.jjangplay.mapper;
+
+import org.apache.ibatis.annotations.Select;
+
+public interface TimeMapper {
+
+	@Select("Select sysdate from dual")
+	public String getTime();
+
+	public String getTime2();
+
+	
+}
